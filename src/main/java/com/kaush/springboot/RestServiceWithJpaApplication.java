12231fff55350b61b6b725c40ff67b8bae733070
kaush <emailtokaush@gmail.com>
2020-12-19 14:12:20 -0500

    added @EnableSwager2 annotation

--- src/main/java/com/kaush/springboot/RestServiceWithJpaApplication.java
+++ src/main/java/com/kaush/springboot/RestServiceWithJpaApplication.java
@@ -3,7 +3,10 @@ package com.kaush.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class RestServiceWithJpaApplication {
 
 	public static void main(String[] args) {
